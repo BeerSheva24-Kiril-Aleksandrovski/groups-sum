5683af6a4d15b16fd7de6702cb05c019ece6c849
Kiril <fanky.yahoo@gmail.com>
2024-12-08 17:43:38 +0200

    HW #46: Solution added

--- src/main/java/telran/numbers/ThreadsPoolGroupSum.java
+++ src/main/java/telran/numbers/ThreadsPoolGroupSum.java
@@ -1,5 +1,8 @@
 package telran.numbers;
 
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 public class ThreadsPoolGroupSum extends ThreadsGroupSum {
@@ -9,7 +12,8 @@ public class ThreadsPoolGroupSum extends ThreadsGroupSum {
 
     @Override
     protected void startTasks(FutureTask<Long>[] tasks) {
-        // TODO
-        // write this method by using threads pool
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        Arrays.stream(tasks).forEach(t -> executor.execute(t));
+        executor.shutdown();
     }
 }
